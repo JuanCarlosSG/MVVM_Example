2fb2c8c6159f4d62c2cd1c2d0d6f5c50e51070b8
Juan Carlos SG <jcsagu00@gmail.com>
2023-03-30 07:11:44 -0600

    Initial Commit

---
+++ MVVM Example/MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_ExampleApp.swift
+//  MVVM Example
+//
+//  Created by Juan Carlos on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
